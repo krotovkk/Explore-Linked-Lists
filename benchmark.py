4a3dc3de0d949348dd7816651faa3a03d4f6df00
Kirill Krotov <krotovkiko@gmail.com>
2019-03-29 01:15:18 +0300

    spaces

--- benchmark.py
+++ benchmark.py
@@ -9,26 +9,26 @@ T  = 1000
 
 def benchmark(func):
 
-	def wrapper():
-		start = time.time()
-		func()
-		end = time.time()
-		print("{:s}: {:f} ms".format(func.__name__,(end - start) * 1000))
-	return wrapper
+    def wrapper():
+        start = time.time()
+        func()
+        end = time.time()
+        print("{:s}: {:f} ms".format(func.__name__,(end - start) * 1000))
+    return wrapper
 
 @benchmark
 def insert_bench():
-	l = linked_list()
-	head = None
-	for i in range(T):
-		data = randint(1, 100)
-		head = l.insert(head, data)
+    l = linked_list()
+    head = None
+    for i in range(T):
+        data = randint(1, 100)
+        head = l.insert(head, data)
 
 @benchmark
 def lib_insert_bench():
-	l = []
-	for i in range(T):
-		l.append(i)
+    l = []
+    for i in range(T):
+        l.append(i)
 
 insert_bench()	
 lib_insert_bench()
